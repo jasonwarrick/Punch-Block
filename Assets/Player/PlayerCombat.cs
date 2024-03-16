aeb4d5e37f3887518e3fa912eda1a5016c4d059e
jasonwarrick <100223827+jasonwarrick@users.noreply.github.com>
2024-03-15 23:25:43 -0400

    Combat system improvements
    It's still not there yet!

--- Assets/Player/PlayerCombat.cs
+++ Assets/Player/PlayerCombat.cs
@@ -4,40 +4,64 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    Animator animator;
+    int maxCombo = 2;
 
     bool idle = false;
-    bool punch = false;
-    bool followUp = false;
+    bool attacking;
+    int comboPos = 0;
+
+    Animator animator;
 
     // Start is called before the first frame update
     void Start() {
         animator = GetComponentInChildren<Animator>();
+
+        UpdateAnimator();
+        idle = comboPos == 0;
     }
 
     // Update is called once per frame
     void Update() {
-        idle = !(punch || followUp);
+        Debug.Log(comboPos == 0);
+        idle = comboPos == 0;
 
         if (Input.GetMouseButtonDown(0)) {
-            if (idle) {
-                punch = true;
-                animator.SetBool("Punching", punch);
-            } else {
-                punch = false;
-                followUp = true;
+            attacking = true;
+
+            if (comboPos == 0) {
+                comboPos ++;
+                Debug.Log("first attack");
+                UpdateAnimator();
+            }
+
+            if (comboPos == maxCombo) {
+                comboPos = 1;
             }
         }
+    }
 
-        // Debug.Log("idle: " + idle + "; punch: " + punch + "; follow up: " + followUp);
+    public void AttackTriggered() {
+        Debug.Log("attack");
+        attacking = false;
     }
 
     public void AttackEnded() {
-        if (!followUp) {
-            punch = false;
-            idle = true;
-            animator.SetBool("Idling", idle);
-            animator.SetBool("Punching", punch);
+        Debug.Log("ended");
+        if (attacking && comboPos > 0) {
+            comboPos++;
+            Debug.Log("Attack queued");
+        } else {
+            attacking = false;
+            comboPos = 0;
+            Debug.Log("Attack not queued");
         }
+
+        UpdateAnimator();
+    }
+
+    void UpdateAnimator() {
+        animator.SetBool("Idling", idle);
+        animator.SetBool("Punching", comboPos == 1);
+        animator.SetBool("FollowingUp", comboPos == 2);
     }
 }
